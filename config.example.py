d7381fcd5ee0f8722767f5f4ab1b164bd7405cfb
tasi788 <tasi788@gmail.com>
2018-05-06 17:46:39 +0800

    set config

---
+++ config.example.py
@@ -0,0 +1,11 @@
+class telegram:
+	token = 'bot:token'
+	uid = [uid]
+
+class transmission:
+	url = 'url'
+	user = ''
+	passwd = ''
+
+class face:
+	emoticon = ['(=ﾟωﾟ)ﾉ', '(´･ω･`)', '(´･_･`)', '(￣▽￣)']
